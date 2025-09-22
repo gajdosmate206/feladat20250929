a4e18344980cfc9381b6851529b3c3f134e8d46f
Nem <Nem@gmail.com>
2025-09-22 10:47:17 +0200

    Commit1

---
+++ react/my-first-react-app/src/compoments/Konyv.jsx
@@ -0,0 +1,12 @@
+function Konyv(){
+        return(
+        <>
+        <p>
+            <span>{props.szerzo}</span>:&nbsp;
+            <span>{props.cim}</span>
+        </p>
+        </>
+    )
+}
+
+export default Konyv;
